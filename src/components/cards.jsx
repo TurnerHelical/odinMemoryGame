961b74dc7d437b743ebf19db9d0de68b27d79a19
TurnerHelical <hunterde91@gmail.com>
2025-09-04 17:48:53 -0400

    testing api calls

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,13 +1,37 @@
+import {React,useState} from 'react'
+
 import '../styles/card.css'
 
 function Cards() {
-    return  <>
-                <div>
-                    <card>
-                        <img></img>
-                        <h4>Name</h4>
-                    </card>
-                </div>
-            </>
+    
+    const getData = async () => {
+        let url = "https://www.dnd5eapi.co/api/2014/monsters/adult-bronze-dragon";
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const result = await response.json();
+            return result
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    const buildArray = async () => {
+        let monsterArray = [];
+        while (monsterArray.length < 13) {
+            let monster = await getData();
+            monsterArray.push(monster)
+        }         
+        console.log(monsterArray);
+    }
+
+    return (
+        <div>
+            <img />
+            <h4 onClick={buildArray}>name</h4>
+        </div>
+    );
 }
-export default Cards
+export default Cards;
