0b943cd7e1e08ec0bb1c8ffffdebce5c50fa8e45
TurnerHelical <hunterde91@gmail.com>
2025-09-05 08:54:37 -0400

    api call works and returns needed data

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,11 +1,11 @@
-import {React,useState} from 'react'
+import {React} from 'react'
 
 import '../styles/card.css'
 
 function Cards() {
     
-    const getData = async () => {
-        let url = "https://www.dnd5eapi.co/api/2014/monsters/adult-bronze-dragon";
+    const getData = async (character) => {
+        let url = `https://www.dnd5eapi.co/api/2014/monsters/${character}`;
         try {
             const response = await fetch(url);
             if (!response.ok) {
@@ -18,17 +18,47 @@ function Cards() {
         }
     };
 
+    const shuffleArray = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]]
+        }
+        return array;
+    }
+
+    const renderCards = async (array) => {
+        const cardCtr = document.querySelector('#cardCtr')
+        for (let item of array) {
+            const card = document.createElement('div');
+            const cardImg = document.createElement('img');
+            const cardText = document.createElement('h4');
+            cardImg.setAttribute('class', 'cardImg');
+            cardImg.setAttribute('src', `${item.image}`);
+            cardText.setAttribute('class', 'cardText');
+            cardText.innerHTML = `${item.name}`;
+            card.appendChild(cardImg);
+            card.appendChild(cardText)
+            cardCtr.appendChild(card);
+        }
+    }
+
     const buildArray = async () => {
+        let monsterNames = ['adult-bronze-dragon', 'air-elemental', 'animated-armor', 'basilisk', 'bandit', 'bugbear', 'centaur', 'dire-wolf', 'djinni', 'drow', 'ghost', 'roc']
         let monsterArray = [];
-        while (monsterArray.length < 13) {
-            let monster = await getData();
-            monsterArray.push(monster)
-        }         
-        console.log(monsterArray);
+        for (let monsterName of monsterNames) {
+            let monster = await getData(monsterName);
+            let monsterObj = {
+                name: monster.name,
+                image: `https://www.dnd5eapi.co${monster.image}` 
+            }
+            monsterArray.push(monsterObj)
+        }             
+        shuffleArray(monsterArray);
+        renderCards(monsterArray);
     }
 
     return (
-        <div>
+        <div id='cardCtr'>
             <img />
             <h4 onClick={buildArray}>name</h4>
         </div>
